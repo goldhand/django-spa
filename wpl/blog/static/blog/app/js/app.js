24e7a493dcf83d13978c3ad96e271231bc1b951c
goldhand <will@webpowerlabs.com>
2013-09-16 21:50:39 -0700

    Animations with angular v1.2.0rc2

--- wpl/blog/static/blog/app/js/app.js
+++ wpl/blog/static/blog/app/js/app.js
@@ -3,12 +3,12 @@
 /* App Module */
 
 //noinspection JSValidateTypes
-angular.module('posts', ['postsServices']).
+angular.module('posts', ['postsServices', 'ngRoute', 'ngAnimate']).
 
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
   $routeProvider.
       //when('/posts', {templateUrl: '/api/blog/angular/partials/post-list/'}).
-      when('/posts/:postId', {templateUrl: '/api/blog/angular/partials/post-list/'}, {controller: 'PostListCtrl'}).
+      when('/posts/:postId', {templateUrl: '/api/blog/angular/partials/post-list/'}).
       otherwise({redirectTo: '/posts'});
     }])
 
